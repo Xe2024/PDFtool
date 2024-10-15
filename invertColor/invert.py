250dabf10f9bf6d0017dad73fc9dcf25daee1129
Xe2024 <rajprasad280323@gmail.com>
2024-10-16 00:10:38 +0530

    fixed path of the resource

--- invertColor/invert.py
+++ invertColor/invert.py
@@ -1,7 +1,7 @@
 # converting the pdf to image
 from pdf2image import convert_from_path
 # Store Pdf with convert_from_path function
-images = convert_from_path('C:/Users/COMPUTER/Downloads/Money And Credit_Shobhit Nirwan.pdf', poppler_path = r'C:\Program Files\poppler-24.08.0\Library\bin')
+images = convert_from_path('./orignalPDF/orignal.pdf', poppler_path = r'C:\Program Files\poppler-24.08.0\Library\bin')
 import PyPDF2
 
 pdfList =[]
@@ -28,11 +28,11 @@ for i in range(len(images)):
     import img2pdf
     import os
     
-    inverted_image.save(f"C:/Users/COMPUTER/Desktop/PDFtools/invertColor/invertedimages/inverted_image{i}.jpg") 
-    inverted_img_path = f"C:/Users/COMPUTER/Desktop/PDFtools/invertColor/invertedimages/inverted_image{i}.jpg"
+    inverted_image.save(f"./invertedimages/inverted_image{i}.jpg") 
+    inverted_img_path = f"./invertedimages/inverted_image{i}.jpg"
     with Image.open(inverted_img_path) as finalimg:
         pdf = img2pdf.convert(finalimg.filename)
-        with open( f"C:/Users/COMPUTER/Desktop/PDFtools/invertColor/pdfs/pdf{i}.pdf" , "wb") as file:
+        with open( f"./pdfs/pdf{i}.pdf" , "wb") as file:
             file.write(pdf)
             print(f"converted {finalimg} to pdf{i}")
             pdfList.append(f"pdf{i}")
@@ -56,12 +56,12 @@ print(pdfList)
 for pdf in pdfList:
   print(pdf)
   # open each pdf
-  with open(f"C:/Users/COMPUTER/Desktop/PDFtools/invertColor/pdfs/{pdf}.pdf", 'rb') as pdfFile:
+  with open(f"./pdfs/{pdf}.pdf", 'rb') as pdfFile:
     # merge each file
     pdfMerge.append(PyPDF2.PdfReader(pdfFile))
 
 # write the merged pdf 
-pdfMerge.write(r'C:\Users\COMPUTER\Desktop\PDFtools\invertColor\finalPDF\final.pdf')
+pdfMerge.write('./finalPDF/final.pdf')
 
 # download the final pdf
 # files.download('merged.pdf')
